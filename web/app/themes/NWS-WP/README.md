74c175ccaba01c228b6ffce9aa2ac83e34a04830
Austin Pickett <pickett.austin@gmail.com>
2018-01-11 18:16:51 -0600

    init

--- web/app/themes/NWS-WP/README.md
+++
@@ -1,2 +0,0 @@
-# NWS-WP
-WP framework ontop of materialize.css, requireJS + angularJS, SASS/Coffeescript
--- web/app/themes/NWS-WP/masthead.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * matties
- *
- * Master Header / Page Header
- */
-?>
-
-<section id="masthead" role="banner"></section>
