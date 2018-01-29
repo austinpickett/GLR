fc72c1b10d05747d6f9d5c7b37ae508e0d4513d3
Austin Pickett <pickett.austin@gmail.com>
2018-01-29 02:35:56 -0600

    Shorthand component

--- web/app/themes/GLR/assets/js/components/search/post.js
+++ web/app/themes/GLR/assets/js/components/search/post.js
@@ -1,30 +1,23 @@
 import React, { PureComponent } from 'react'
 
-export default class Post extends PureComponent {
-    constructor(props) {
-        super(props)
-    }
-    render() { 
-        console.log(this.props.data._embedded)
 
-        return (
-        <article
-            className="article" 
-            role="article" 
-            itemScope 
-            itemType="http://schema.org/BlogPosting"
-        >
-            <figure>
-                <img src={this.props.data._embedded['wp:featuredmedia'][0].source_url} />
-            </figure>
+const Post = props => 
+    <article
+        className="article" 
+        role="article" 
+        itemScope 
+        itemType="http://schema.org/BlogPosting"
+    >
+        <figure>
+            <img src={this.props.data._embedded['wp:featuredmedia'][0].source_url} />
+        </figure>
 
-            <div className="excerpt">
-                <div className="meta">{this.props.data._embedded.author[0].name} | {this.props.data.date}</div>
-                <a href={this.props.data.link}>{this.props.data.title.rendered}</a>
+        <div className="excerpt">
+            <div className="meta">{this.props.data._embedded.author[0].name} | {this.props.data.date}</div>
+            <a href={this.props.data.link}>{this.props.data.title.rendered}</a>
 
-                <div className="tag">{this.props.data._embedded['wp:term'][0][0].name}</div>
-            </div>
-        </article>
-        )
-    }
-}
+            <div className="tag">{this.props.data._embedded['wp:term'][0][0].name}</div>
+        </div>
+    </article>
+
+export default Post
