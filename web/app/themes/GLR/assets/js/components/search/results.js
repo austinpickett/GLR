c417a5b1e16150f4d6427a05c22373c564290748
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-01-29 20:48:17 -0600

    Update results.js

--- web/app/themes/GLR/assets/js/components/search/results.js
+++ web/app/themes/GLR/assets/js/components/search/results.js
@@ -1,13 +1,11 @@
-import React, { PureComponent } from 'react'
 import Post from './post'
 
-const Results = props => 
-    <div className="the-posts row wrapper news">
-        <div className="articles">
-            {props.results.map(x => {
-                return <Post data={x} key={Math.random()} />
-            })}
-        </div>
-    </div>
-
-export default Results
+export default ({ results }) => (
+  <div className="the-posts row wrapper news">
+      <div className="articles">
+          {results.map(x => {
+              return <Post data={x} key={Math.random()} />
+          })}
+      </div>
+  </div>
+)
